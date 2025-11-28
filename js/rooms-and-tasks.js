bf027f590d1dd76aa9d78be15ee4467f01038238
aaron-ferguson <aaron@newheights.coach>
2025-11-28 09:39:14 -0700

    Switch rooms and tasks back to typical house config Minor tweaks to rooms and tasks.

--- js/rooms-and-tasks.js
+++ js/rooms-and-tasks.js
@@ -1,5 +1,4 @@
 // Game data structure
-/*
 const ROOMS_AND_TASKS = {
 "Anywhere": [
 "Power Down Protocol: Close your eyes, stand still, and count out loud for 30 seconds."
@@ -7,7 +6,7 @@ const ROOMS_AND_TASKS = {
 "Outside": [
 "Stellar Navigation: Look at the sky for 20 seconds.",
 "Suspicious Surveillance: Stand in front of a neighbor's house for 20 seconds.",
-"House Boundary Scan: Touch all four outside corners of the house.",
+"Boundary Scan: Touch all four outside corners of the house.",
 "Prepare the Runway: Lay down and roll across the driveway.",
 "Touch Grass: Remove your footwear and stand on the grass for 5 seconds.",
 "Test Comms: Put your head in the mailbox for 15 seconds."
@@ -33,11 +32,11 @@ const ROOMS_AND_TASKS = {
 "Bedrooms": [
 "Sleep Reset: Lie face down on a bed for 20 seconds.",
 "Meditation Moment: Assume a meditative pose and hold it for 20 seconds.",
-"Initialization Routine: Do 1 pushup, 1 sit-up, and 1 jumping jack."
+"Initialization Routine: Do 10 pushups, 10 sit-ups, and 10 jumping jacks."
 ],
 "Bathrooms": [
-"Emergency Water Reset: Turn both faucets on and off.",
-"Sit in a bathtub for 15 seconds."
+"Emergency Water Reset: Touch the sink for 20 seconds.",
+"Sit in a bathtub for 20 seconds."
 ],
 "Closets": [
 "Spend 10 seconds in three different closets. The door must be closed."
@@ -50,8 +49,8 @@ const ROOMS_AND_TASKS = {
 "Put your head in the dryer for 10 seconds."
 ]
 };
-*/
 
+/*
 // This set of rooms and tasks is temporary until we can store different sets and users. 
 const ROOMS_AND_TASKS = {
 "Anywhere": [
@@ -60,21 +59,20 @@ const ROOMS_AND_TASKS = {
 "Outside": [
 "Stellar Navigation: Look at the sky for 20 seconds.",
 "Suspicious Surveillance: Stand in front of someone else's car for 20 seconds.",
-"House Boundary Scan: Touch all four outside corners of the church.",
-"Clear the Way: Go up and down the stairs 3 times.",
+"Boundary Scan: Touch all four outside corners of the church.",
+"Clear the Way: Go from the building doors to the parking lot 3 times.",
 "Touch Grass: Remove your footwear and stand on the grass for 5 seconds.",
 "Vehicle Diagnostics: Place your hand on a car tire for 20 seconds."
 ],
 "Cultural Hall": [
 "Gymnasium Groove: Dance in the living room for 20 seconds.",
-"Remote Frequency Calibration: Find the TV remote and press any button.",
 "Light Sync: Turn each light switch in the room off, then on again."
 ],
 "Kitchen": [
 "Heat Sensor Check: Open the oven/microwave door for 20 seconds, then close it.",
 "Cold Storage Audit: Open the fridge and name three items out loud.",
 "Utensil Sort: Use utensils to spell the word 'ALLY' and then return them.",
-"Water Pressure Test: Run the sink for 3 seconds.",
+"Water Pressure Test: Touch the sink for 20 seconds.",
 "Nutrient Calibration: Touch 3 different types of food and name them out loud."
 ],
 "Foyer": [
@@ -89,13 +87,14 @@ const ROOMS_AND_TASKS = {
 "Primary Room": [
 "Musical Chairs: Set up and take down 5 different folding chairs",
 "Meditation Moment: Assume a meditative pose and hold it for 20 seconds.",
-"Initialization Routine: Do 1 pushup, 1 sit-up, and 1 jumping jack."
+"Initialization Routine: Do 10 pushups, 10 sit-ups, and 10 jumping jacks."
 ],
 "Nursery": [
 "Singing Time: Sing a nursery rhyme from start to finish.",
 "Play Time: Play with a toy for 20 seconds."
 ]
 };
+*/
 
 // Export for testing and module usage
 export { ROOMS_AND_TASKS };
